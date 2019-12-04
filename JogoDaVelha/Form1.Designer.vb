a98cc266b122c6d0ed1a43cac94c12b97dd414a4
Weskley Moreira <49239094+weskleyM@users.noreply.github.com>
2019-12-04 06:18:12 -0300

    Add files via upload
    Jogo da Velha

---
+++ JogoDaVelha/Form1.Designer.vb
@@ -0,0 +1,294 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class JogoVelha
+    Inherits System.Windows.Forms.Form
+
+    'Descartar substituições de formulário para limpar a lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Exigido pelo Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'OBSERVAÇÃO: o procedimento a seguir é exigido pelo Windows Form Designer
+    'Pode ser modificado usando o Windows Form Designer.  
+    'Não o modifique usando o editor de códigos.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.pnl1 = New System.Windows.Forms.Panel()
+        Me.lbl2 = New System.Windows.Forms.Label()
+        Me.lblPlay = New System.Windows.Forms.Label()
+        Me.pnl2 = New System.Windows.Forms.Panel()
+        Me.btn9 = New System.Windows.Forms.Button()
+        Me.btn8 = New System.Windows.Forms.Button()
+        Me.btn7 = New System.Windows.Forms.Button()
+        Me.btn6 = New System.Windows.Forms.Button()
+        Me.btn5 = New System.Windows.Forms.Button()
+        Me.btn4 = New System.Windows.Forms.Button()
+        Me.btn3 = New System.Windows.Forms.Button()
+        Me.btn2 = New System.Windows.Forms.Button()
+        Me.btn1 = New System.Windows.Forms.Button()
+        Me.btnNovo = New System.Windows.Forms.Button()
+        Me.pnl1.SuspendLayout()
+        Me.pnl2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'pnl1
+        '
+        Me.pnl1.BackColor = System.Drawing.Color.White
+        Me.pnl1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.pnl1.Controls.Add(Me.lbl2)
+        Me.pnl1.Controls.Add(Me.lblPlay)
+        Me.pnl1.Location = New System.Drawing.Point(20, 18)
+        Me.pnl1.Name = "pnl1"
+        Me.pnl1.Size = New System.Drawing.Size(289, 83)
+        Me.pnl1.TabIndex = 0
+        '
+        'lbl2
+        '
+        Me.lbl2.AutoSize = True
+        Me.lbl2.Font = New System.Drawing.Font("Microsoft Sans Serif", 19.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbl2.Location = New System.Drawing.Point(158, 16)
+        Me.lbl2.Name = "lbl2"
+        Me.lbl2.Size = New System.Drawing.Size(79, 38)
+        Me.lbl2.TabIndex = 1
+        Me.lbl2.Text = "1(X)"
+        '
+        'lblPlay
+        '
+        Me.lblPlay.AutoSize = True
+        Me.lblPlay.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblPlay.Location = New System.Drawing.Point(43, 21)
+        Me.lblPlay.Name = "lblPlay"
+        Me.lblPlay.Size = New System.Drawing.Size(126, 32)
+        Me.lblPlay.TabIndex = 0
+        Me.lblPlay.Text = "Jogador:"
+        '
+        'pnl2
+        '
+        Me.pnl2.BackColor = System.Drawing.Color.Black
+        Me.pnl2.Controls.Add(Me.btn9)
+        Me.pnl2.Controls.Add(Me.btn8)
+        Me.pnl2.Controls.Add(Me.btn7)
+        Me.pnl2.Controls.Add(Me.btn6)
+        Me.pnl2.Controls.Add(Me.btn5)
+        Me.pnl2.Controls.Add(Me.btn4)
+        Me.pnl2.Controls.Add(Me.btn3)
+        Me.pnl2.Controls.Add(Me.btn2)
+        Me.pnl2.Controls.Add(Me.btn1)
+        Me.pnl2.Location = New System.Drawing.Point(20, 129)
+        Me.pnl2.Name = "pnl2"
+        Me.pnl2.Size = New System.Drawing.Size(289, 236)
+        Me.pnl2.TabIndex = 1
+        '
+        'btn9
+        '
+        Me.btn9.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.btn9.BackColor = System.Drawing.Color.White
+        Me.btn9.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btn9.FlatAppearance.BorderSize = 0
+        Me.btn9.FlatAppearance.CheckedBackColor = System.Drawing.Color.White
+        Me.btn9.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White
+        Me.btn9.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.btn9.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn9.Font = New System.Drawing.Font("Consolas", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn9.Location = New System.Drawing.Point(203, 171)
+        Me.btn9.Name = "btn9"
+        Me.btn9.Size = New System.Drawing.Size(85, 64)
+        Me.btn9.TabIndex = 8
+        Me.btn9.UseVisualStyleBackColor = False
+        '
+        'btn8
+        '
+        Me.btn8.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.btn8.BackColor = System.Drawing.Color.White
+        Me.btn8.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btn8.FlatAppearance.BorderSize = 0
+        Me.btn8.FlatAppearance.CheckedBackColor = System.Drawing.Color.White
+        Me.btn8.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White
+        Me.btn8.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.btn8.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn8.Font = New System.Drawing.Font("Consolas", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn8.Location = New System.Drawing.Point(101, 171)
+        Me.btn8.Name = "btn8"
+        Me.btn8.Size = New System.Drawing.Size(85, 64)
+        Me.btn8.TabIndex = 7
+        Me.btn8.UseVisualStyleBackColor = False
+        '
+        'btn7
+        '
+        Me.btn7.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.btn7.BackColor = System.Drawing.Color.White
+        Me.btn7.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btn7.FlatAppearance.BorderSize = 0
+        Me.btn7.FlatAppearance.CheckedBackColor = System.Drawing.Color.White
+        Me.btn7.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White
+        Me.btn7.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.btn7.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn7.Font = New System.Drawing.Font("Consolas", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn7.Location = New System.Drawing.Point(1, 171)
+        Me.btn7.Name = "btn7"
+        Me.btn7.Size = New System.Drawing.Size(85, 64)
+        Me.btn7.TabIndex = 6
+        Me.btn7.UseVisualStyleBackColor = False
+        '
+        'btn6
+        '
+        Me.btn6.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.btn6.BackColor = System.Drawing.Color.White
+        Me.btn6.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btn6.FlatAppearance.BorderSize = 0
+        Me.btn6.FlatAppearance.CheckedBackColor = System.Drawing.Color.White
+        Me.btn6.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White
+        Me.btn6.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.btn6.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn6.Font = New System.Drawing.Font("Consolas", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn6.Location = New System.Drawing.Point(203, 84)
+        Me.btn6.Name = "btn6"
+        Me.btn6.Size = New System.Drawing.Size(85, 64)
+        Me.btn6.TabIndex = 5
+        Me.btn6.UseVisualStyleBackColor = False
+        '
+        'btn5
+        '
+        Me.btn5.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.btn5.BackColor = System.Drawing.Color.White
+        Me.btn5.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btn5.FlatAppearance.BorderSize = 0
+        Me.btn5.FlatAppearance.CheckedBackColor = System.Drawing.Color.White
+        Me.btn5.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White
+        Me.btn5.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.btn5.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn5.Font = New System.Drawing.Font("Consolas", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn5.Location = New System.Drawing.Point(101, 84)
+        Me.btn5.Name = "btn5"
+        Me.btn5.Size = New System.Drawing.Size(85, 64)
+        Me.btn5.TabIndex = 4
+        Me.btn5.UseVisualStyleBackColor = False
+        '
+        'btn4
+        '
+        Me.btn4.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.btn4.BackColor = System.Drawing.Color.White
+        Me.btn4.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btn4.FlatAppearance.BorderSize = 0
+        Me.btn4.FlatAppearance.CheckedBackColor = System.Drawing.Color.White
+        Me.btn4.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White
+        Me.btn4.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.btn4.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn4.Font = New System.Drawing.Font("Consolas", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn4.Location = New System.Drawing.Point(1, 84)
+        Me.btn4.Name = "btn4"
+        Me.btn4.Size = New System.Drawing.Size(85, 64)
+        Me.btn4.TabIndex = 3
+        Me.btn4.UseVisualStyleBackColor = False
+        '
+        'btn3
+        '
+        Me.btn3.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.btn3.BackColor = System.Drawing.Color.White
+        Me.btn3.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btn3.FlatAppearance.BorderSize = 0
+        Me.btn3.FlatAppearance.CheckedBackColor = System.Drawing.Color.White
+        Me.btn3.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White
+        Me.btn3.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.btn3.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn3.Font = New System.Drawing.Font("Consolas", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn3.Location = New System.Drawing.Point(203, 1)
+        Me.btn3.Name = "btn3"
+        Me.btn3.Size = New System.Drawing.Size(85, 64)
+        Me.btn3.TabIndex = 2
+        Me.btn3.UseVisualStyleBackColor = False
+        '
+        'btn2
+        '
+        Me.btn2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.btn2.BackColor = System.Drawing.Color.White
+        Me.btn2.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btn2.FlatAppearance.BorderSize = 0
+        Me.btn2.FlatAppearance.CheckedBackColor = System.Drawing.Color.White
+        Me.btn2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White
+        Me.btn2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.btn2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn2.Font = New System.Drawing.Font("Consolas", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn2.Location = New System.Drawing.Point(101, 1)
+        Me.btn2.Name = "btn2"
+        Me.btn2.Size = New System.Drawing.Size(85, 64)
+        Me.btn2.TabIndex = 1
+        Me.btn2.UseVisualStyleBackColor = False
+        '
+        'btn1
+        '
+        Me.btn1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.btn1.BackColor = System.Drawing.Color.White
+        Me.btn1.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btn1.FlatAppearance.BorderSize = 0
+        Me.btn1.FlatAppearance.CheckedBackColor = System.Drawing.Color.White
+        Me.btn1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White
+        Me.btn1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.btn1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btn1.Font = New System.Drawing.Font("Consolas", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btn1.Location = New System.Drawing.Point(1, 1)
+        Me.btn1.Name = "btn1"
+        Me.btn1.Size = New System.Drawing.Size(85, 64)
+        Me.btn1.TabIndex = 0
+        Me.btn1.UseVisualStyleBackColor = False
+        '
+        'btnNovo
+        '
+        Me.btnNovo.BackColor = System.Drawing.Color.Lime
+        Me.btnNovo.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnNovo.FlatAppearance.BorderColor = System.Drawing.Color.Black
+        Me.btnNovo.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnNovo.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnNovo.ForeColor = System.Drawing.Color.White
+        Me.btnNovo.Location = New System.Drawing.Point(62, 395)
+        Me.btnNovo.Name = "btnNovo"
+        Me.btnNovo.Size = New System.Drawing.Size(196, 47)
+        Me.btnNovo.TabIndex = 2
+        Me.btnNovo.Text = "Novo Jogo"
+        Me.btnNovo.UseVisualStyleBackColor = False
+        '
+        'JogoVelha
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(330, 459)
+        Me.Controls.Add(Me.btnNovo)
+        Me.Controls.Add(Me.pnl2)
+        Me.Controls.Add(Me.pnl1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.Name = "JogoVelha"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Jogo da Velha"
+        Me.pnl1.ResumeLayout(False)
+        Me.pnl1.PerformLayout()
+        Me.pnl2.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents pnl1 As Panel
+    Friend WithEvents pnl2 As Panel
+    Friend WithEvents btn9 As Button
+    Friend WithEvents btn8 As Button
+    Friend WithEvents btn7 As Button
+    Friend WithEvents btn6 As Button
+    Friend WithEvents btn5 As Button
+    Friend WithEvents btn4 As Button
+    Friend WithEvents btn3 As Button
+    Friend WithEvents btn2 As Button
+    Friend WithEvents btn1 As Button
+    Friend WithEvents lblPlay As Label
+    Friend WithEvents lbl2 As Label
+    Friend WithEvents btnNovo As Button
+End Class
---
+++ JogoDaVelha/Form1.vb
@@ -0,0 +1,101 @@
+﻿Public Class JogoVelha
+    Private Sub JogoVelha_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        For Each c As Control In pnl2.Controls
+            If c.GetType() = GetType(Button) Then
+                AddHandler c.Click, AddressOf btn_Click
+            End If
+        Next
+    End Sub
+
+    Dim xo As Integer = 0
+    Private Sub btn_Click(sender As Object, e As EventArgs)
+        Dim btn As Button = sender
+        If btn.Text.Equals("") Then
+            If xo Mod 2 = 0 Then
+                btn.Text = "X"
+                btn.ForeColor = Color.Red
+                lbl2.Text = "2(O)"
+                vencedor()
+            Else
+                btn.Text = "O"
+                btn.ForeColor = Color.Blue
+                lbl2.Text = "1(X)"
+                vencedor()
+            End If
+            xo += 1
+        End If
+    End Sub
+
+    Dim vict As Boolean = False
+    Private Sub vencedor()
+        'Fileiras Horizontais
+        If Not btn1.Text.Equals("") AndAlso btn1.Text.Equals(btn2.Text) AndAlso btn2.Text.Equals(btn3.Text) Then
+            vict = True
+            msgVencedor(btn1, btn2, btn3)
+        ElseIf Not btn4.Text.Equals("") AndAlso btn4.Text.Equals(btn5.Text) AndAlso btn5.Text.Equals(btn6.Text) Then
+            vict = True
+            msgVencedor(btn4, btn5, btn6)
+        ElseIf Not btn7.Text.Equals("") AndAlso btn7.Text.Equals(btn8.Text) AndAlso btn8.Text.Equals(btn9.Text) Then
+            vict = True
+            msgVencedor(btn7, btn8, btn9)
+
+            'Fileiras Verticais
+        ElseIf Not btn1.Text.Equals("") AndAlso btn1.Text.Equals(btn4.Text) AndAlso btn4.Text.Equals(btn7.Text) Then
+            vict = True
+            msgVencedor(btn1, btn4, btn7)
+        ElseIf Not btn2.Text.Equals("") AndAlso btn2.Text.Equals(btn5.Text) AndAlso btn5.Text.Equals(btn8.Text) Then
+            vict = True
+            msgVencedor(btn2, btn5, btn8)
+        ElseIf Not btn3.Text.Equals("") AndAlso btn3.Text.Equals(btn6.Text) AndAlso btn6.Text.Equals(btn9.Text) Then
+            vict = True
+            msgVencedor(btn3, btn6, btn9)
+
+            'Fileiras Diagonais
+        ElseIf Not btn1.Text.Equals("") AndAlso btn1.Text.Equals(btn5.Text) AndAlso btn5.Text.Equals(btn9.Text) Then
+            vict = True
+            msgVencedor(btn1, btn5, btn9)
+        ElseIf Not btn3.Text.Equals("") AndAlso btn3.Text.Equals(btn5.Text) AndAlso btn5.Text.Equals(btn7.Text) Then
+            vict = True
+            msgVencedor(btn3, btn5, btn7)
+        End If
+        'Todos botões preenchidos e não houver vencedor
+        If btnsPreenchidos() = 9 AndAlso vict = False Then
+            MsgBox("Sem Vencedor")
+        End If
+    End Sub
+
+    Function btnsPreenchidos() As Integer
+        Dim btns As Integer = 0
+        For Each c As Control In pnl2.Controls
+            If c.GetType() = GetType(Button) Then
+                btns += c.Text.Length
+            End If
+        Next
+        Return btns
+    End Function
+
+    Private Sub msgVencedor(ByVal b1 As Button, ByVal b2 As Button, ByVal b3 As Button)
+        b1.BackColor = Color.GreenYellow
+        b2.BackColor = Color.GreenYellow
+        b3.BackColor = Color.GreenYellow
+        If b1.Text.Equals("X") AndAlso b1.Text.Equals(b2.Text) AndAlso b2.Text.Equals(b3.Text) Then
+            MsgBox("Jogador 1 venceu")
+        ElseIf b1.Text.Equals("O") AndAlso b1.Text.Equals(b2.Text) AndAlso b2.Text.Equals(b3.Text) Then
+            MsgBox("Jogador 2 venceu")
+        End If
+
+    End Sub
+
+    Private Sub btnNovo_Click(sender As Object, e As EventArgs) Handles btnNovo.Click
+        xo = 0
+        vict = False
+        lbl2.Text = "1(X)"
+        For Each c As Control In pnl2.Controls
+            If c.GetType() = GetType(Button) Then
+                c.BackColor = Color.White
+                c.Text = ""
+            End If
+        Next
+    End Sub
+
+End Class
