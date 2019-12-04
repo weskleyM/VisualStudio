9d5f05e33e46918838f9a7166e6d61b136c36ed2
Weskley Moreira <49239094+weskleyM@users.noreply.github.com>
2019-12-04 06:15:15 -0300

    Add files via upload
    Hello World

---
+++ Hello/Form1.Designer.vb
@@ -0,0 +1,67 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class HelloWorld
+    Inherits System.Windows.Forms.Form
+
+    'Descartar substituições de formulário para limpar a lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Exigido pelo Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'OBSERVAÇÃO: o procedimento a seguir é exigido pelo Windows Form Designer
+    'Pode ser modificado usando o Windows Form Designer.  
+    'Não o modifique usando o editor de códigos.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblHello = New System.Windows.Forms.Label()
+        Me.btnClique = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lblHello
+        '
+        Me.lblHello.AutoSize = True
+        Me.lblHello.Font = New System.Drawing.Font("Comic Sans MS", 30.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblHello.Location = New System.Drawing.Point(230, 145)
+        Me.lblHello.Name = "lblHello"
+        Me.lblHello.Size = New System.Drawing.Size(0, 71)
+        Me.lblHello.TabIndex = 0
+        '
+        'btnClique
+        '
+        Me.btnClique.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnClique.Location = New System.Drawing.Point(290, 316)
+        Me.btnClique.Name = "btnClique"
+        Me.btnClique.Size = New System.Drawing.Size(188, 66)
+        Me.btnClique.TabIndex = 1
+        Me.btnClique.Text = "Clique Aqui!"
+        Me.btnClique.UseVisualStyleBackColor = True
+        '
+        'HelloWorld
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btnClique)
+        Me.Controls.Add(Me.lblHello)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.Name = "HelloWorld"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Hello"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblHello As Label
+    Friend WithEvents btnClique As Button
+End Class
---
+++ Hello/Form1.vb
@@ -0,0 +1,6 @@
+﻿Public Class HelloWorld
+    Private Sub btnClique_Click(sender As Object, e As EventArgs) Handles btnClique.Click
+        Dim texto As String = "Hello World!"
+        lblHello.Text = texto
+    End Sub
+End Class
