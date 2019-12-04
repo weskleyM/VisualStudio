ef895a56c57e1f60612ed47b9fe6a684e62d4cdd
Weskley Moreira <49239094+weskleyM@users.noreply.github.com>
2019-12-04 06:16:37 -0300

    Add files via upload
    Calculadora

---
+++ Teste/Form1.Designer.vb
@@ -0,0 +1,122 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmCalc
+    Inherits System.Windows.Forms.Form
+
+    'Descartar substituições de formulário para limpar a lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Exigido pelo Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'OBSERVAÇÃO: o procedimento a seguir é exigido pelo Windows Form Designer
+    'Pode ser modificado usando o Windows Form Designer.  
+    'Não o modifique usando o editor de códigos.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtNum1 = New System.Windows.Forms.TextBox()
+        Me.txtNum2 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.btnSoma = New System.Windows.Forms.Button()
+        Me.btnSub = New System.Windows.Forms.Button()
+        Me.lblSimb = New System.Windows.Forms.Label()
+        Me.lblRes = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'txtNum1
+        '
+        Me.txtNum1.Location = New System.Drawing.Point(25, 81)
+        Me.txtNum1.Name = "txtNum1"
+        Me.txtNum1.Size = New System.Drawing.Size(100, 22)
+        Me.txtNum1.TabIndex = 0
+        '
+        'txtNum2
+        '
+        Me.txtNum2.Location = New System.Drawing.Point(186, 81)
+        Me.txtNum2.Name = "txtNum2"
+        Me.txtNum2.Size = New System.Drawing.Size(100, 22)
+        Me.txtNum2.TabIndex = 1
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(339, 85)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(16, 17)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "="
+        '
+        'btnSoma
+        '
+        Me.btnSoma.Location = New System.Drawing.Point(312, 139)
+        Me.btnSoma.Name = "btnSoma"
+        Me.btnSoma.Size = New System.Drawing.Size(75, 23)
+        Me.btnSoma.TabIndex = 5
+        Me.btnSoma.Text = "+"
+        Me.btnSoma.UseVisualStyleBackColor = True
+        '
+        'btnSub
+        '
+        Me.btnSub.Location = New System.Drawing.Point(312, 178)
+        Me.btnSub.Name = "btnSub"
+        Me.btnSub.Size = New System.Drawing.Size(75, 23)
+        Me.btnSub.TabIndex = 6
+        Me.btnSub.Text = "-"
+        Me.btnSub.UseVisualStyleBackColor = True
+        '
+        'lblSimb
+        '
+        Me.lblSimb.AutoSize = True
+        Me.lblSimb.Location = New System.Drawing.Point(143, 85)
+        Me.lblSimb.Name = "lblSimb"
+        Me.lblSimb.Size = New System.Drawing.Size(0, 17)
+        Me.lblSimb.TabIndex = 7
+        '
+        'lblRes
+        '
+        Me.lblRes.AutoSize = True
+        Me.lblRes.Location = New System.Drawing.Point(404, 85)
+        Me.lblRes.Name = "lblRes"
+        Me.lblRes.Size = New System.Drawing.Size(28, 17)
+        Me.lblRes.TabIndex = 8
+        Me.lblRes.Text = "0.0"
+        '
+        'frmCalc
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.Control
+        Me.ClientSize = New System.Drawing.Size(544, 219)
+        Me.Controls.Add(Me.lblRes)
+        Me.Controls.Add(Me.lblSimb)
+        Me.Controls.Add(Me.btnSub)
+        Me.Controls.Add(Me.btnSoma)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtNum2)
+        Me.Controls.Add(Me.txtNum1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.Name = "frmCalc"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Calculadora"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtNum1 As TextBox
+    Friend WithEvents txtNum2 As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents btnSoma As Button
+    Friend WithEvents btnSub As Button
+    Friend WithEvents lblSimb As Label
+    Friend WithEvents lblRes As Label
+End Class
---
+++ Teste/Form1.vb
@@ -0,0 +1,50 @@
+﻿Public Class frmCalc
+    Public num1 As Double
+    Public num2 As Double
+    Public res As Double
+
+    Private Sub txtNum1_KeyPress(sender As Object, x As EventArgs) Handles txtNum1.KeyPress
+        apenasNum(x)
+    End Sub
+
+    Private Sub txtNum2_KeyPress(sender As Object, x As EventArgs) Handles txtNum2.KeyPress
+        apenasNum(x)
+    End Sub
+
+    Public Sub apenasNum(ByVal x As System.Windows.Forms.KeyPressEventArgs)
+        If (Asc(x.KeyChar) >= 48 And Asc(x.KeyChar) <= 57) Or Asc(x.KeyChar) = 46 Or Asc(x.KeyChar) = 8 Then
+            'Asc(x.KeyChar) >= 48 And Asc(x.KeyChar) <= 57 // Permite apenas a entrada de 0 a 9
+            'Asc(x.KeyChar) = 46 // Permite a entrada do ponto "."
+            'Asc(x.KeyChar) = 8 // Permite apagar usando o backspace
+        Else
+            x.Handled = True
+            'MsgBox("Apenas Numeros")
+        End If
+    End Sub
+
+    Private Sub btnSoma_Click(sender As Object, e As EventArgs) Handles btnSoma.Click
+        If (txtNum1.Text.Equals("")) Then
+            MsgBox("Preencha o campos")
+        Else
+            num1 = Val(txtNum1.Text)
+            num2 = Val(txtNum2.Text)
+            res = num1 + num2
+            lblRes.Text = res
+            txtNum1.Text = ""
+            txtNum2.Text = ""
+        End If
+    End Sub
+
+    Private Sub btnSub_Click(sender As Object, e As EventArgs) Handles btnSub.Click
+        If (txtNum1.Text.Equals("")) Then
+            MsgBox("Preencha o campos")
+        Else
+            num1 = Val(txtNum1.Text)
+            num2 = Val(txtNum2.Text)
+            res = num1 - num2
+            lblRes.Text = res
+            txtNum1.Text = ""
+            txtNum2.Text = ""
+        End If
+    End Sub
+End Class
